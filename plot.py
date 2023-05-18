5dea4853cf785d96fbd617fba78b7a86ec127fc9
Hayden Housen <hayden@haydenhousen.com>
2023-05-18 15:52:54 -0400

    Plot two runs

--- plot.py
+++ plot.py
@@ -2,14 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-with open("dreamer-boxworld-logs/metrics.jsonl", "r") as metrics_file:
-    metrics = [json.loads(line) for line in metrics_file]
-    data = []
-    for metric in metrics:
-        try:
-            data.append(metric["episode/score"])
-        except KeyError:
-            pass
-    data = np.convolve(data, np.ones((500,))/500, mode='valid')
-    plt.plot(data)
-    plt.savefig("plot.png")
+
+for number in [16, 64]:
+    print(f"Plotting {number}...")
+    with open(f"dreamer-boxworld-scale_big_train{number}/metrics.jsonl", "r") as metrics_file:
+        metrics = [json.loads(line) for line in metrics_file]
+        data = []
+        for metric in metrics:
+            try:
+                data.append(metric["episode/score"])
+            except KeyError:
+                pass
+        data = np.convolve(np.array(data) >= 10, np.ones((500,))/500, mode='valid')
+        plt.plot(data)
+        plt.savefig(f"plot{number}.png")
